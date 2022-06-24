fe870c682853414ed904afbb32e52e1dce682364
raulbenavides27 <ra.benavides@duocuc.cl>
2022-06-24 01:45:54 -0400

    actualizando

--- arte_todo/views.py
+++ arte_todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request);
-return render(request,'arte_todo/index.html')
+def home(request):
+return render(request,'arte_todo/home.html')
