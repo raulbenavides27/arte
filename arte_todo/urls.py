a4ba73a9c53b78ab836289c998d7551931394f7f
raulbenavides27 <ra.benavides@duocuc.cl>
2022-06-25 14:49:28 -0400

    actualizado con vistas templ.

--- arte_todo/urls.py
+++ arte_todo/urls.py
@@ -1,6 +1,11 @@
 
 from django.urls import path
-from .views import home
+from .views import home, contacto, galeria, comunidad
 urlpatterns = [
     path('',home, name="home"),
+    path('contacto/', contacto, name="contacto"),
+    path('galeria/', galeria, name="galeria"),
+    path('comunidad/', comunidad, name="comunidad"),
+    
+
 ]
--- arte_todo/views.py
+++ arte_todo/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
      return render(request,'arte_todo/home.html')
+
+def contacto(request):
+     return render(request,'arte_todo/contacto.html')
+
+def galeria(request):
+     return render(request,'arte_todo/galeria.html')
+
+def comunidad(request):
+     return render(request,'arte_todo/comunidad.html')
