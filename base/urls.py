c3b22bf773ac2e8442c3e018e7775b83d7fc8bc0
raulbenavides27 <ra.benavides@duocuc.cl>
2022-06-27 14:49:53 -0400

    correciones y cambios

--- base/urls.py
+++ base/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('arte_todo.urls')),
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
