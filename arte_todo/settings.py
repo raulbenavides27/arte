424fb3ab64cc7cbfaed4ddc99a9e9fc6d626a116
raulbenavides27 <ra.benavides@duocuc.cl>
2022-07-01 23:49:39 -0400

    a  presentar

--- arte_todo/settings.py
+++ arte_todo/settings.py
@@ -27,10 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
  
-MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+
 
 # Application definition
 
--- base/settings.py
+++ base/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
