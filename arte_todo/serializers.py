66902646ebd020d65e472b184746ee6d69e6ac14
raulbenavides27 <ra.benavides@duocuc.cl>
2022-07-12 16:56:41 -0400

    actualizando api validadndo nombre de foto

--- arte_todo/serializers.py
+++ arte_todo/serializers.py
@@ -2,6 +2,13 @@ from .models import foto
 from rest_framework import serializers
 
 class fotoSerializer(serializers.ModelSerializer):
+    
+    def validate_nombre(self, value):
+        
+        existe = foto.objects.filter(nombre=value).exists()
+        if existe:
+            raise serializers.ValidationError("este nombre ya esta uso")
+        return value
     class Meta:
         model = foto
         fields = '__all__'
