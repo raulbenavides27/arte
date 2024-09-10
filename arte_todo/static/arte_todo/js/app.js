5931d6f214597a0702a4dae105acb215b710fcca
raulbenavides27 <ra.benavides@duocuc.cl>
2024-09-10 17:14:00 -0300

    datos

--- arte_todo/static/arte_todo/js/app.js
+++ arte_todo/static/arte_todo/js/app.js
@@ -1,23 +1,23 @@
-const imagen1= document.getElementById('imagen1');
-const imagen2= document.getElementById('imagen2');
-const imagen3= document.getElementById('imagen3');
+const imagen1 = document.getElementById('imagen1');
+const imagen2 = document.getElementById('imagen2');
+const imagen3 = document.getElementById('imagen3');
 
-const cargarImagen = (entradas,observador) => {
-    
+const cargarImagen = (entradas, observador) => {
     entradas.forEach((entrada) => {
-        if(entrada.isIntersecting){
+        if (entrada.isIntersecting) {
             entrada.target.classList.add('visible'); 
-        }else{
+        } else {
             entrada.target.classList.remove('visible'); 
         }
     });
 }
 
-const observador = new IntersectionObserver(cargarImagen,{
+const observador = new IntersectionObserver(cargarImagen, {
     root: null,
-    rootMargin:'0px 0px 0px 0px',
-    threshold:1.7 
+    rootMargin: '0px',
+    threshold: 0.5 // Cambiado para mejor comportamiento
 });
+
 observador.observe(imagen1);
 observador.observe(imagen2);
 observador.observe(imagen3);
